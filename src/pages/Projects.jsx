097e1d24229799624a87653a6a4d666e352df6ab
m1guelbazan <garciabazanmiguel@gmail.com>
2024-01-28 14:02:28 -0800

    setting up project page

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,12 @@
+mport React from "react";
+
+const About = () => (
+  <div>
+    <h1 className="title is-1">This is the About Page</h1>
+    <p>
+      Projects section
+    </p>
+  </div>
+);
+
+export default About;
