d85ae0760c2f3c9cf6c09792b1af19375fe4d7a6
m1guelbazan <garciabazanmiguel@gmail.com>
2024-02-12 20:37:10 -0800

    added files for button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -43,7 +43,7 @@ class Navbar extends Component {
         return(
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">
-                    Miguel Bazan <i className="fab fa-react"></i>
+                    Miguel <i className="fab fa-react"></i>
                 </h1>
                 <div className="menu-icon" onClick={this.handleClick}>
                     <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}> </i>
